a303ed12988c1a5dadd920e2417ad43fd46309e3
agazzarini <a.gazzarini@gmail.com>
2017-11-20 15:55:03 +0100

    First import

---
+++ src/main/java/org/folio/rest/impl/BaseResource.java
@@ -0,0 +1,93 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLClient;
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Session;
+import net.sf.hibernate.SessionFactory;
+import org.folio.cataloging.integration.PieceOfExistingLogicAdapter;
+import org.folio.cataloging.log.Log;
+import org.folio.cataloging.log.MessageCatalog;
+import org.folio.cataloging.log.PublicMessageCatalog;
+import org.folio.rest.client.ConfigurationsClient;
+import org.folio.rest.jaxrs.model.LogicalViewCollection;
+import org.folio.rest.jaxrs.resource.LogicalViewsResource;
+import org.folio.rest.tools.utils.TenantTool;
+
+import javax.ws.rs.core.Response;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import static org.folio.cataloging.F.datasourceConfiguration;
+import static org.folio.cataloging.Global.HCONFIGURATION;
+
+public abstract class BaseResource {
+    protected final Log logger = new Log(getClass());
+
+    public void doGet(
+            final PieceOfExistingLogicAdapter adapter,
+            final Handler<AsyncResult<LogicalViewCollection>> resultHandler,
+            final Handler<AsyncResult<Response>> asyncResultHandler,
+            final Map<String, String> okapiHeaders,
+            final Context ctx) throws Exception {
+        final ConfigurationsClient configuration =
+                new ConfigurationsClient(
+                        "127.0.0.1",
+                        8085,
+                        TenantTool.tenantId(okapiHeaders));
+
+        configuration.getEntries("module==CATALOGING and configName==datasource", 0, 4, "en", response -> {
+            response.bodyHandler(body -> {
+                final SQLClient client = JDBCClient.createShared(ctx.owner(), datasourceConfiguration(body));
+                client.getConnection(operation -> {
+                    ctx.executeBlocking(
+                            future -> {
+                                Session session = null;
+                                try (final Connection connection = operation.result().unwrap()) {
+                                    final SessionFactory factory = HCONFIGURATION.buildSessionFactory();
+                                    session = factory.openSession(connection);
+
+                                    adapter.execute(session, future);
+
+                                } catch (final SQLException exception) {
+                                    logger.error(MessageCatalog._00010_DATA_ACCESS_FAILURE, exception);
+                                    asyncResultHandler.handle(
+                                            Future.succeededFuture(
+                                                    LogicalViewsResource.GetLogicalViewsResponse.withPlainInternalServerError(
+                                                            PublicMessageCatalog.INTERNAL_SERVER_ERROR)));
+                                } catch (final Exception exception) {
+                                    logger.error(MessageCatalog._00011_NWS_FAILURE, exception);
+                                    asyncResultHandler.handle(
+                                            Future.succeededFuture(
+                                                    LogicalViewsResource.GetLogicalViewsResponse.withPlainInternalServerError(
+                                                            PublicMessageCatalog.INTERNAL_SERVER_ERROR)));
+                                } finally {
+                                    if (session != null) {
+                                        try {
+                                            session.close();
+                                        } catch (final HibernateException ignore) {
+                                            // Ignore
+                                        }
+                                    }
+                                }
+                            },
+                            false,
+                            resultHandler);
+                });
+            });
+        });
+    }
+
+
+    public Response internalServerError(final String message) {
+        return Response.status(500)
+                .header("Content-Type", "application/json")
+                .entity(message)
+                .build();
+    }
+}
--- src/main/java/org/folio/rest/impl/LogicalViewAPI.java
+++ src/main/java/org/folio/rest/impl/LogicalViewAPI.java
@@ -8,6 +8,9 @@ import io.vertx.core.Handler;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.SQLClient;
 import static librisuite.bean.cataloguing.bibliographic.codelist.CodeListsBean.getDatabaseViewList;
+
+import librisuite.business.codetable.ValueLabelElement;
+import librisuite.business.common.DataAccessException;
 import librisuite.hibernate.DB_LIST;
 import net.sf.hibernate.HibernateException;
 import net.sf.hibernate.Session;
@@ -25,29 +28,37 @@ import org.folio.rest.tools.utils.TenantTool;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
 import static org.folio.cataloging.F.datasourceConfiguration;
 import static org.folio.cataloging.Global.HCONFIGURATION;
 
-public class LogicalViewAPI implements LogicalViewsResource {
+public class LogicalViewAPI extends BaseResource implements LogicalViewsResource {
     private final static Log LOGGER = new Log(LogicalViewAPI.class);
 
+    private Function<ValueLabelElement, View> adapter = source -> {
+        final View logicalView = new View();
+        logicalView.setCode(source.getValue());
+        logicalView.setLongDescription(source.getLabel());
+        return logicalView;
+    };
+
     @Override
-    public void getLogicalViews(String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
-        doGet( (session, future) -> {
+    public void getLogicalViews(
+            final String lang,
+            final Map<String, String> okapiHeaders,
+            final Handler<AsyncResult<Response>> resultHandler,
+            final Context vertxContext) throws Exception {
+        doGet((session, future) -> {
             try {
                 final LogicalViewCollection container = new LogicalViewCollection();
                 container.setViews(
-                        getDatabaseViewList().getDAO().getList(session, DB_LIST.class, Locale.forLanguageTag(lang)).stream()
-                                .map(nvp -> {
-                                    final View logicalView = new View();
-                                    logicalView.setCode(nvp.getValue());
-                                    logicalView.setLongDescription(nvp.getLabel());
-                                    return logicalView;
-                                })
+                        logicalViews(lang, session).stream()
+                                .map(adapter)
                                 .collect(toList()));
                 future.complete(container);
             } catch (final Exception exception) {
@@ -56,16 +67,20 @@ public class LogicalViewAPI implements LogicalViewsResource {
             }
         }, operation -> {
             if (operation.succeeded()) {
-                asyncResultHandler.handle(
+                resultHandler.handle(
                         Future.succeededFuture(
                                 GetLogicalViewsResponse.withJsonOK(operation.result())));
             } else {
-                asyncResultHandler.handle(
+                resultHandler.handle(
                         Future.succeededFuture(
-                                GetLogicalViewsResponse.withPlainInternalServerError(
+                                internalServerError(
                                         PublicMessageCatalog.INTERNAL_SERVER_ERROR)));
             }
-        },asyncResultHandler, okapiHeaders, vertxContext);
+        }, resultHandler, okapiHeaders, vertxContext);
+    }
+
+    public List<ValueLabelElement> logicalViews(final String lang, final Session session) throws DataAccessException {
+        return getDatabaseViewList().getDAO().getList(session, DB_LIST.class, Locale.forLanguageTag(lang));
     }
 
     @Override
@@ -85,60 +100,6 @@ public class LogicalViewAPI implements LogicalViewsResource {
 
     @Override
     public void putLogicalViewsByViewId(String viewId, String lang, View entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
-
-    }
-
-    public void doGet(
-        final PieceOfExistingLogicAdapter adapter,
-        final Handler<AsyncResult<LogicalViewCollection>> resultHandler,
-        final Handler<AsyncResult<Response>> asyncResultHandler,
-        final Map<String, String> okapiHeaders,
-        final Context ctx) throws Exception {
-        final ConfigurationsClient configuration =
-                new ConfigurationsClient(
-                        "127.0.0.1",
-                        8085,
-                        TenantTool.tenantId(okapiHeaders));
-
-        configuration.getEntries("module==CATALOGING and configName==datasource", 0, 4, "en", response -> {
-            response.bodyHandler(body -> {
-                final SQLClient client = JDBCClient.createShared(ctx.owner(), datasourceConfiguration(body));
-                client.getConnection(operation -> {
-                        ctx.executeBlocking(
-                                future -> {
-                                    Session session = null;
-                                    try (final Connection connection = operation.result().unwrap()) {
-                                        final SessionFactory factory = HCONFIGURATION.buildSessionFactory();
-                                        session = factory.openSession(connection);
-
-                                        adapter.execute(session, future);
-
-                                    } catch (final SQLException exception) {
-                                        LOGGER.error(MessageCatalog._00010_DATA_ACCESS_FAILURE, exception);
-                                        asyncResultHandler.handle(
-                                                Future.succeededFuture(
-                                                        GetLogicalViewsResponse.withPlainInternalServerError(
-                                                                PublicMessageCatalog.INTERNAL_SERVER_ERROR)));
-                                    } catch (final Exception exception) {
-                                        LOGGER.error(MessageCatalog._00011_NWS_FAILURE, exception);
-                                        asyncResultHandler.handle(
-                                                Future.succeededFuture(
-                                                        GetLogicalViewsResponse.withPlainInternalServerError(
-                                                                PublicMessageCatalog.INTERNAL_SERVER_ERROR)));
-                                    } finally {
-                                        if (session != null) {
-                                            try {
-                                                session.close();
-                                            } catch (final HibernateException ignore) {
-                                                // Ignore
-                                            }
-                                        }
-                                    }
-                                },
-                                false,
-                                resultHandler);
-                });
-            });
-        });
+        throw new IllegalArgumentException();
     }
 }
