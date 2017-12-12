c4cea18340108f850c798bf374404586c490a3b7
carmentrazza <carmen.trazza@atcult.it>
2017-12-12 15:34:08 +0100

    MODCAT-7: Created the service that returns the index with the corresponding constraints and the service that returns a list of all indexes belonging to such category

--- src/main/java/org/folio/cataloging/dao/persistence/IndexSub.java
+++ src/main/java/org/folio/cataloging/dao/persistence/IndexSub.java
@@ -3,8 +3,8 @@ package org.folio.cataloging.dao.persistence;
 import java.io.Serializable;
 
 /**
- * @author Hansv
- * @version $Revision: 1.1 $, $Date: 2005/09/02 13:18:08 $ 
+ * @author carment
+ * @since 1.0
  */
 public class IndexSub implements Serializable {
 	private int listKey;
