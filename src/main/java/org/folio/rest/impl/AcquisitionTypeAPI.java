0652c5f295742468a1e461ea4c7beaac1cfc7385
alice <alice.guercio@atcult.it>
2018-01-08 12:06:45 +0100

    MODCAT-19: AcquisitionType API

---
+++ src/main/java/org/folio/rest/impl/AcquisitionTypeAPI.java
@@ -0,0 +1,72 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import org.folio.cataloging.business.codetable.ValueLabelElement;
+import org.folio.cataloging.log.Log;
+import org.folio.cataloging.log.MessageCatalog;
+import org.folio.rest.jaxrs.model.AcquisitionType;
+import org.folio.rest.jaxrs.model.AcquisitionTypeCollection;
+import org.folio.rest.jaxrs.resource.AcquisitionTypesResource;
+
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+import static org.folio.cataloging.integration.CatalogingHelper.doGet;
+
+/**
+ * Note Types RESTful APIs.
+ *
+ * @author aguercio
+ * @since 1.0
+ */
+
+public class AcquisitionTypeAPI implements AcquisitionTypesResource{
+
+    protected final Log logger = new Log(AcquisitionTypeAPI.class);
+
+    // This is the adapter that converts existing value objects (logical views in this case)
+    // in OKAPI resources.
+    private Function<ValueLabelElement, AcquisitionType> toAcquisitionType = source -> {
+        final AcquisitionType acquisitionType = new AcquisitionType();
+        //TODO: handle type Integer for value element or null value in Integer.parseInt
+        acquisitionType.setCode(Integer.parseInt(source.getValue()));
+        acquisitionType.setDescription(source.getLabel());
+        return acquisitionType;
+    };
+
+    @Override
+    public void getAcquisitionTypes(String lang,
+                                    Map<String, String> okapiHeaders,
+                                    Handler<AsyncResult<Response>> asyncResultHandler,
+                                    Context vertxContext) throws Exception {
+        doGet((storageService, future) -> {
+            try {
+                final AcquisitionTypeCollection container = new AcquisitionTypeCollection();
+                container.setAcquisitionTypes(
+                        storageService.getAcquisitionTypes(lang)
+                                .stream()
+                                .map(toAcquisitionType)
+                                .collect(toList()));
+                return container;
+            } catch (final Exception exception) {
+                logger.error(MessageCatalog._00010_DATA_ACCESS_FAILURE, exception);
+                return null;
+            }
+        }, asyncResultHandler, okapiHeaders, vertxContext);
+
+    }
+
+    @Override
+    public void postAcquisitionTypes(String lang,
+                                     AcquisitionType entity,
+                                     Map<String, String> okapiHeaders,
+                                     Handler<AsyncResult<Response>> asyncResultHandler,
+                                     Context vertxContext) throws Exception {
+        throw new IllegalArgumentException();
+
+    }
+}
