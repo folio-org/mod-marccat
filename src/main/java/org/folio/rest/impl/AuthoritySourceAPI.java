7da44ea644de6bd32a8c982fb31915d0b13e3767
natasciab <natascia.bianchini@atcult.it>
2018-01-08 12:11:49 +0100

    [ MODCAT-16 ] Authority source management.

---
+++ src/main/java/org/folio/rest/impl/AuthoritySourceAPI.java
@@ -0,0 +1,73 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import org.folio.cataloging.business.codetable.ValueLabelElement;
+import org.folio.cataloging.log.Log;
+import org.folio.cataloging.log.MessageCatalog;
+import org.folio.rest.jaxrs.model.AuthoritySource;
+import org.folio.rest.jaxrs.model.AuthoritySourceCollection;
+import org.folio.rest.jaxrs.resource.AuthoritySourcesResource;
+
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+import static org.folio.cataloging.integration.CatalogingHelper.doGet;
+
+/**
+ * Authority Sources RESTful APIs.
+ *
+ * @author natasciab
+ * @since 1.0
+ */
+
+public class AuthoritySourceAPI implements AuthoritySourcesResource {
+
+    protected final Log logger = new Log(AuthoritySourceAPI.class);
+
+    // This is the adapter that converts existing value objects (authority source in this case)
+    // in OKAPI resources.
+    private Function<ValueLabelElement, AuthoritySource> toAuthoritySource = source -> {
+        final AuthoritySource authoritySource = new AuthoritySource();
+        //TODO: handle type Integer for value element or null value in Integer.parseInt
+        authoritySource.setCode(Integer.parseInt(source.getValue()));
+        authoritySource.setDescription(source.getLabel());
+        return authoritySource;
+    };
+
+    @Override
+    public void getAuthoritySources(final String lang,
+                                    final Map<String, String> okapiHeaders,
+                                    final Handler<AsyncResult<Response>> asyncResultHandler,
+                                    final Context vertxContext) throws Exception {
+
+        doGet((storageService, future) -> {
+            try {
+                final AuthoritySourceCollection container = new AuthoritySourceCollection();
+                container.setAuthoritySources(
+                        storageService.getAuthoritySources(lang)
+                                .stream()
+                                .map(toAuthoritySource)
+                                .collect(toList()));
+                return container;
+            } catch (final Exception exception) {
+                logger.error(MessageCatalog._00010_DATA_ACCESS_FAILURE, exception);
+                return null;
+            }
+        }, asyncResultHandler, okapiHeaders, vertxContext);
+
+    }
+
+    @Override
+    public void postAuthoritySources(final String lang,
+                                     final AuthoritySource entity,
+                                     final Map<String, String> okapiHeaders,
+                                     final Handler<AsyncResult<Response>> asyncResultHandler,
+                                     final Context vertxContext) throws Exception {
+        throw new IllegalArgumentException();
+
+    }
+}
