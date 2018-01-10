f35cf8dae81b2712b399dd76f50c3c570e9aed97
carmentrazza <carmen.trazza@atcult.it>
2018-01-10 14:12:26 +0100

    MODCAT-18:Add Material Type

---
+++ src/main/java/org/folio/rest/impl/MaterialTypesAPI.java
@@ -0,0 +1,64 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import org.folio.cataloging.business.codetable.Avp;
+import org.folio.cataloging.log.Log;
+import org.folio.cataloging.log.MessageCatalog;
+
+import org.folio.rest.jaxrs.model.MaterialType;
+import org.folio.rest.jaxrs.model.MaterialTypeCollection;
+import org.folio.rest.jaxrs.resource.MaterialTypesResource;
+
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+import static org.folio.cataloging.integration.CatalogingHelper.doGet;
+
+/**
+ * ShelfList type RESTful APIs.
+ *
+ * @author natasciab
+ * @since 1.0
+ */
+
+public class MaterialTypesAPI implements MaterialTypesResource {
+
+    protected final Log logger = new Log(MaterialTypesAPI.class);
+
+    private Function<Avp<String>, MaterialType> toMaterialType = source -> {
+        final MaterialType materialType = new MaterialType();
+        materialType.setCode(source.getValue());
+        materialType.setDescription(source.getLabel());
+        return materialType;
+    };
+
+    @Override
+    public void getMaterialTypes(final String lang,
+                                  final Map<String, String> okapiHeaders,
+                                  final Handler<AsyncResult<Response>> asyncResultHandler,
+                                  final Context vertxContext) throws Exception {
+        doGet((storageService, future) -> {
+            try {
+                final MaterialTypeCollection container = new MaterialTypeCollection();
+                container.setMaterialTypes(
+                        storageService.getMaterialTypes(lang)
+                                .stream()
+                                .map(toMaterialType)
+                                .collect(toList()));
+                return container;
+            } catch (final Exception exception) {
+                logger.error(MessageCatalog._00010_DATA_ACCESS_FAILURE, exception);
+                return null;
+            }
+        }, asyncResultHandler, okapiHeaders, vertxContext);
+    }
+
+    @Override
+    public void postMaterialTypes(String lang, MaterialType entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+        throw new IllegalArgumentException();
+    }
+}
