c8debb1699034d9bf7537195e4a7009a0f0f981c
Christian Chiama <christian.chiama@atcult.it>
2018-09-17 19:38:23 +0200

    error on port
    error on port

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #author christian chiama
 echo "kill previous deploy....."
-PORT_NUMBER=8080
+PORT_NUMBER=8889
 lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
 echo "killing........."
 sleep 3
 echo "Deploy new artifact........."
-ohup java -Dserver.port=8080 -jar ./target/mod-cataloging-1.0.jar &
+ohup java -Dserver.port=8889 -jar ./target/mod-cataloging-1.0.jar &
