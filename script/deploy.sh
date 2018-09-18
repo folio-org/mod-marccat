39250a7e900c4a87567352eaaa399f62a78054b4
Christian Chiama <christian.chiama@atcult.it>
2018-09-18 11:56:22 +0200

    add test
    add test

---
+++ script/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#author christian chiama
+echo "kill previous deploy....."
+PORT_NUMBER=8889
+lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill
+echo "killing........."
+sleep 3
+echo "Deploy new artifact........."
+nohup java -Dserver.port=8889 -jar ./target/mod-cataloging-1.0.jar &
