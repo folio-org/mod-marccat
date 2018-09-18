a25991b1290cdd364ede09682defeead5aaa30aa
Christian Chiama <christian.chiama@atcult.it>
2018-09-18 13:06:34 +0200

    change param for succesfull test
    change param for succesfull test

--- src/test/java/org/folio/cataloging/it/TemplateManagementTestCase.java
+++ src/test/java/org/folio/cataloging/it/TemplateManagementTestCase.java
@@ -1,4 +1,39 @@
 package org.folio.cataloging.it;
 
-public class TemplateManagementTestCase {
+import org.folio.cataloging.resources.domain.FieldCollection;
+import org.folio.cataloging.resources.domain.RecordTemplateCollection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class TemplateManagementTestCase extends BaseIntegrationTestCase {
+  /**
+   * Setup fixture for this integration test case.
+   */
+  @Before
+  public void setUp() {
+    super.setUp();
+    get(address("/record-templates?type=B"), RecordTemplateCollection.class)
+      .getRecordTemplates()
+      .forEach(template -> {
+        delete(address("/record-template/" + template.getId() + "?type=B"));
+      });
+
+    assertEquals(0, get(address("/record-templates?type=B"), RecordTemplateCollection.class)
+      .getRecordTemplates().size());
+  }
+
+  @Test
+  public void createNewTemplate() {
+    final FieldCollection mandatoryFields = get(address("/bibliographic/fields/mandatory"), FieldCollection.class);
+    System.out.println(mandatoryFields.getFields().size());
+  }
 }
