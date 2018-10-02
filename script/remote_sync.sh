f381ef51ea038441aaad8a02e294d3a235aa71a1
Christian Chiama <christian.chiama@atcult.it>
2018-10-02 17:42:20 +0200

    change script path
    change script path

--- script/remote_sync.sh
+++ script/remote_sync.sh
@@ -6,5 +6,37 @@
 # Version: 1.0
 # Date   : 01/10/2018
 #########################################################
-sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
 
+SSH_SRC_FILE=./target/mod-cataloging-1.0.jar
+CFG_FILE=folio_ctl.conf
+RESTART_WAIT_SEC=3
+
+check_env(){
+  if [ -e ../conf/${CFG_FILE} ]
+  then
+    . ../conf/${CFG_FILE}
+  else
+    echo "FATAL ERROR: File ${CFG_FILE} not found."
+    exit 1
+  fi
+}
+
+rem_sync(){
+  DATE=`date`
+  scp -P ${SSH_PORT} ${SSH_SRC_FILE} ${SSH_USR}@${SSH_HOST}:${SSH_DEST_FILE}
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -eq 0 ]
+  then
+    nohup echo "cd folio/bin ; sh stop_modcat.sh ; sleep ${RESTART_WAIT_SEC} ; sh start_modcat.sh" | ssh -p ${SSH_PORT} ${SSH_USR}@${SSH_HOST} &
+  else
+    echo "ERROR - cannot upload file ${SSH_SRC_FILE} via ssh to host ${SSH_HOST} [${EXIT_CODE}]"
+  fi
+}
+
+go(){
+  check_env
+  rem_sync
+}
+
+go
