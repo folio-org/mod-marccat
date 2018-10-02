45f35f4727d25c357b3e950793f451648825c191
Christian Chiama <christian.chiama@atcult.it>
2018-10-02 16:57:18 +0200

    test deploy su ITNET
    test deploy su ITNET

---
+++ script/remote_sync.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+########################################################
+# Synchronise remote test Folio mod-cataloging
+#
+# Author : Mirko Fonzo - mirko.fonzo@atcult.it
+# Version: 1.0
+# Date   : 01/10/2018
+#########################################################
+sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
+
