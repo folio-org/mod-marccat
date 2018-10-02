7df15da88e3293d40fbfc1f4e5f266eb0d9b375b
Christian Chiama <christian.chiama@atcult.it>
2018-10-02 17:16:34 +0200

    remove sudo
    remove sudo

--- script/remote_sync.sh
+++ script/remote_sync.sh
@@ -6,5 +6,5 @@
 # Version: 1.0
 # Date   : 01/10/2018
 #########################################################
-sudo sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
+sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
 
