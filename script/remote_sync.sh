bc951d548ace0f55d4080ac978b489f9bfd4f88c
Christian Chiama <christian.chiama@atcult.it>
2018-10-02 17:28:32 +0200

    test deploy
    test deploy

--- script/remote_sync.sh
+++ script/remote_sync.sh
@@ -6,4 +6,5 @@
 # Version: 1.0
 # Date   : 01/10/2018
 #########################################################
-sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
+cd /home/folio/folio/bin/
+sh sync_rem_env.sh ./target/mod-cataloging-1.0.jar
