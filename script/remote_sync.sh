e44e0885aba419c909566ea26e0e3c01f02e0bb6
Christian Chiama <christian.chiama@atcult.it>
2018-10-02 17:07:47 +0200

    add sudo permission
    add sudo permission

--- script/remote_sync.sh
+++ script/remote_sync.sh
@@ -2,9 +2,9 @@
 ########################################################
 # Synchronise remote test Folio mod-cataloging
 #
-# Author : Mirko Fonzo - mirko.fonzo@atcult.it
+# Author : Christian Chiama - christian.chiama@atcult.it
 # Version: 1.0
 # Date   : 01/10/2018
 #########################################################
-sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
+sudo sh /home/folio/folio/bin/sync_rem_env.sh ./target/mod-cataloging-1.0.jar
 
