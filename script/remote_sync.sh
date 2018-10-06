61145e491af6588791529d167e5e4bbf073ed330
Christian Chiama <christian.chiama@atcult.it>
2018-10-06 02:10:36 +0200

    test remote deploy
    test remote deploy

--- script/remote_sync.sh
+++ script/remote_sync.sh
@@ -16,20 +16,7 @@ RESTART_WAIT_SEC=3
 
 
 rem_sync(){
-  DATE=`date`
-  scp -P ${SSH_PORT} ${SSH_SRC_FILE} ${SSH_USR}@${SSH_HOST}:${SSH_DEST_FILE}
-  EXIT_CODE=$?
-
-  if [ $EXIT_CODE -eq 0 ]
-  then
-    nohup echo "cd folio/bin ; sh stop_modcat.sh ; sleep ${RESTART_WAIT_SEC} ; sh start_modcat.sh" | ssh -p ${SSH_PORT} ${SSH_USR}@${SSH_HOST} &
-  else
-    echo "ERROR - cannot upload file ${SSH_SRC_FILE} via ssh to host ${SSH_HOST} [${EXIT_CODE}]"
-  fi
+scp -P 22022 ${SSH_SRC_FILE} folio@151.1.165.20:/usr/local/folio/data
+echo "cd /usr/local/folio/bin ; sh deploy_mod-cat.sh" | ssh -p 22022 folio@151.1.165.20
 }
-
-go(){
-  rem_sync
-}
-
-go
+rem_sync
