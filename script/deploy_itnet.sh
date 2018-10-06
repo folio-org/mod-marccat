ed0efc12365b475a125a8a40b922bc9b65adbade
Christian Chiama <christian.chiama@atcult.it>
2018-10-06 02:31:48 +0200

    clean code
    clean code

--- script/deploy_itnet.sh
+++ script/deploy_itnet.sh
@@ -11,13 +11,15 @@ SSH_SRC_FILE=./target/mod-cataloging-1.0.jar
 SSH_HOST=151.1.165.20
 SSH_PORT=22022
 SSH_USR=folio
-SSH_DEST_FILE=/tmp/
+SSH_DEST_FILE=/usr/local/folio/data
 RESTART_WAIT_SEC=3
 
 
 rem_sync(){
-  scp -P 22022 ${SSH_SRC_FILE} folio@151.1.165.20:/usr/local/folio/data
+  echo "uploading artifact via ssh on ITNET"
+  scp -P ${SSH_PORT} ${SSH_SRC_FILE} folio@${SSH_HOST}:${SSH_DEST_FILE}
   sleep ${RESTART_WAIT_SEC}
-  nohup echo "cd /usr/local/folio/bin ; sh deploy_mod-cat.sh" | ssh -p 22022 folio@151.1.165.20 &
+  nohup echo "cd /usr/local/folio/bin ; sh deploy_mod-cat.sh" | ssh -p ${SSH_PORT} folio@${SSH_HOST} &
+  echo "Deploy new artifact........."
 }
 rem_sync
