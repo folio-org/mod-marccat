5c4def0db64c58a6f8e3fdfbdd106e5066a9a8e3
Christian Chiama <christian.chiama@atcult.it>
2018-10-26 19:39:51 +0200

    fix
    fix

--- src/main/java/org/folio/cataloging/resources/BaseResource.java
+++ src/main/java/org/folio/cataloging/resources/BaseResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(origins = {
+  "http://folio.atcult.it:3000",
   "http://151.1.163.20:3000",
   "http://151.1.165.20:3000",
   "http://localhost:3000"
