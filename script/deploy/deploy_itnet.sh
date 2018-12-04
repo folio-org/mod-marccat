57b36e3e483b11b10a179e6011a62e91bc665cfc
Christian Chiama <christian.chiama@atcult.it>
2018-12-04 11:46:52 +0100

    revert changes
    revert changes

--- script/deploy/deploy_itnet.sh
+++ script/deploy/deploy_itnet.sh
@@ -22,7 +22,7 @@ ssh_itnet_deploy(){
   scp -P ${SSH_PORT} ${SSH_SRC_FILE} folio@${SSH_HOST}:${SSH_DEST_FILE}
   sleep ${RESTART_WAIT_SEC}
   echo "uploaded artifact succesfully. Deploy last release of modcat"
-  nohup echo "cd /usr/local/folio/data ; java -jar mod-marccat-1.0.0-SNAPSHOT.jar " | ssh -p ${SSH_PORT} folio@${SSH_HOST} &
+  nohup echo "cd /usr/local/folio/bin ; sh deploy_mod-cat.sh" | ssh -p ${SSH_PORT} folio@${SSH_HOST} &
 }
 ssh_itnet_deploy
 
