577d4a38411de71807bab3498b056329d1473695
Christian Chiama <39725134+cchiama@users.noreply.github.com>
2019-01-07 00:45:39 +0100

    Update deploy_itnet.sh

--- script/deploy/deploy_itnet.sh
+++ script/deploy/deploy_itnet.sh
@@ -7,7 +7,7 @@
 # Date   : 01/10/2018
 #########################################################
 
-SSH_SRC_FILE=./target/mod-marccat-1.1.0.jar
+SSH_SRC_FILE=./target/mod-marccat-1.2.0.jar
 SSH_HOST=151.1.165.20
 SSH_HOST_DEMO=151.1.163.20
 SSH_PORT=22022
@@ -22,7 +22,7 @@ ssh_itnet_deploy(){
   scp -P ${SSH_PORT} ${SSH_SRC_FILE} folio@${SSH_HOST}:${SSH_DEST_FILE}
   sleep ${RESTART_WAIT_SEC}
   echo "uploaded artifact succesfully. Deploy last release of modcat"
-  nohup echo "cd /usr/local/folio/data ; java -jar mod-marccat-1.1.0.jar " | ssh -p ${SSH_PORT} folio@${SSH_HOST} &
+  nohup echo "cd /usr/local/folio/data ; java -jar mod-marccat-1.2.0.jar " | ssh -p ${SSH_PORT} folio@${SSH_HOST} &
 }
 ssh_itnet_deploy
 
