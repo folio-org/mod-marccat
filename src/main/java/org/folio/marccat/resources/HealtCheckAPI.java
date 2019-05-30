6b38a6e3ea82a6986bdb8db408ff52e5b9f14e60
Christian Chiama <christian.chiama@atcult.it>
2019-05-30 12:25:36 +0200

    add test API
    add test API

--- src/main/java/org/folio/marccat/resources/HealtCheckAPI.java
+++ src/main/java/org/folio/marccat/resources/HealtCheckAPI.java
@@ -1,4 +1,19 @@
 package org.folio.marccat.resources;
 
-public class HealtCheckAPI {
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealtCheckAPI extends BaseResource<JSONObject> {
+
+  /**
+   * Gets the count of the record.
+   */
+  @GetMapping("/checkOkapi")
+  public JSONObject checkOkapi() {
+   return new JSONObject(new String[]{"say","hello"});
+  }
+
+
 }
