6311addfa9cd4d3bfbed6444f2910c7703c0fbc8
carmen <carmen.trazza@atcult.it>
2019-07-12 11:06:21 +0200

    Change configurations and write log

--- src/main/java/org/folio/marccat/resources/TenantAPI.java
+++ src/main/java/org/folio/marccat/resources/TenantAPI.java
@@ -18,7 +18,7 @@ public class TenantAPI {
 
   @PostMapping
   public ResponseEntity<String> create(
-    @RequestHeader(Global.OKAPI_TENANT_HEADER_NAME)  String tenant,
+    @RequestHeader(name = Global.OKAPI_TENANT_HEADER_NAME, defaultValue = "tnx")   String tenant,
     @RequestBody TenantAttributes attributes
   ) throws SQLException, IOException {
     tenantService.createTenant(tenant);
