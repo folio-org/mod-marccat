6ecc79c0075ef26c336747bf20b3920f6ffd5c0a
Matteo Pascucci <matteo.pascucci@atcult.it>
2019-09-05 15:38:49 +0200

    revert
    revert

--- src/main/java/org/folio/marccat/ModMarccat.java
+++ src/main/java/org/folio/marccat/ModMarccat.java
@@ -1,9 +1,5 @@
 package org.folio.marccat;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import org.folio.marccat.config.log.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -18,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 
 public class ModMarccat {
 
-  private static final Log logger = new Log(ModMarccat.class);
   public static final String BASE_URI = "marccat";
 
   /**
@@ -27,15 +22,6 @@ public class ModMarccat {
    * @param args the command line arguments.
    */
   public static void main(final String[] args) {
-    InetAddress inetAddress = null;
-	try {
-		inetAddress = InetAddress.getLocalHost();
-		logger.info("OKAPI_URL: "+ System.getProperty("okapiurl"));
-		logger.info("IP Address-localhost:- " + inetAddress);
-		logger.info("IP Address-host:- " + inetAddress.getHostAddress());
-	} catch (UnknownHostException e) {
-		e.printStackTrace();
-	}
     SpringApplication.run(ModMarccat.class, args);
   }
 
