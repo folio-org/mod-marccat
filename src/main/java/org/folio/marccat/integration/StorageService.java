df75e8ff12022eca350a6996132e300621da1981
carmen <carmen.trazza@atcult.it>
2019-11-19 13:47:24 +0100

    Fix browse

--- src/main/java/org/folio/marccat/integration/StorageService.java
+++ src/main/java/org/folio/marccat/integration/StorageService.java
@@ -604,7 +604,7 @@ public class StorageService implements Closeable {
       browseTerm = descriptor.getSortForm();
 
       descriptorsList = dao.getHeadingsBySortform("<", "desc", browseTerm, filter, view, 1, session);
-      if (!(dao instanceof PublisherDescriptorDAO) && descriptorsList.isEmpty()) {
+      if (!(dao instanceof PublisherDescriptorDAO) && !descriptorsList.isEmpty()) {
         browseTerm = dao.getBrowsingSortForm(descriptorsList.get(0));
         descriptorsList.clear();
       }
@@ -1052,8 +1052,8 @@ public class StorageService implements Closeable {
           if (dao instanceof ShelfListDAO) {
             filter = filter + HDG_MAIN_LIBRARY_NUMBER + mainLibrary;
           }
-          browseTerm = descriptor.getDisplayText();
-          browseTerm = descriptor.getSortForm();/*dao.calculateSearchTerm(browseTerm, key, session);*/
+          descriptor.calculateAndSetSortForm();
+          browseTerm = descriptor.getSortForm();
 
           if (dao instanceof PublisherDescriptorDAO || dao instanceof NameTitleNameDescriptorDAO)
             operator = ">=";
