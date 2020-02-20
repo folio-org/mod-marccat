fb14ec6d2ae9a101231186041c2e09cd05c05908
carmen <carmen.trazza@atcult.it>
2020-02-20 10:30:55 +0100

    Fix loading

--- src/main/java/org/folio/marccat/integration/tools/TenantLoading.java
+++ src/main/java/org/folio/marccat/integration/tools/TenantLoading.java
@@ -202,7 +202,7 @@ public class TenantLoading {
         httpHeaders.add(Global.OKAPI_URL, headers.get(Global.OKAPI_TENANT_HEADER_NAME));
         httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
         MultiValueMap <String, Object> requestEntity = new LinkedMultiValueMap <>();
-        File file = getResourceAsFile("/sample-data/load-from-file/record1.mrc");
+        File file = getResourceAsFile("/sample-data/load-from-file/records.mrc");
         requestEntity.add("files", new FileSystemResource(file));
         HttpEntity <MultiValueMap <String, Object>> httpEntity = new HttpEntity <>(requestEntity, httpHeaders);
         client.exchange(
