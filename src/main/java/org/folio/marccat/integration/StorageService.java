19624024b9aa5cfbd57cd7b540f9d084b920d52c
carmen <carmen.trazza@atcult.it>
2020-04-24 10:07:11 +0200

    MODCAT-146 Fix tag 006-Book

--- src/main/java/org/folio/marccat/integration/StorageService.java
+++ src/main/java/org/folio/marccat/integration/StorageService.java
@@ -1390,7 +1390,7 @@ public class StorageService implements Closeable {
         || status == Field.FieldStatus.CHANGED) {
 
         if (tagNbr.equals(Global.OTHER_MATERIAL_TAG_CODE)) {
-          final Map<String, Object> mapRecordTypeMaterial = getMaterialTypeInfosByLeaderValues(field.getFixedField().getMaterialTypeCode().charAt(0),' ', tagNbr);
+          final Map<String, Object> mapRecordTypeMaterial = getHeaderTypeByRecordTypeCode(field.getFixedField().getMaterialTypeCode().charAt(0));
           final String formOfMaterial = (String) mapRecordTypeMaterial.get(Global.FORM_OF_MATERIAL_LABEL);
           recordParser.changeMaterialDescriptionOtherTag(item, field, formOfMaterial, generalInformation);
         }
