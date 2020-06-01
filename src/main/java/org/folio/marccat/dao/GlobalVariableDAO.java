d4dd6012d6b67959cf914f6f956eca87d1157eba
carmen <carmen.trazza@atcult.it>
2020-06-01 11:05:00 +0200

    Fix MODCAT-165

---
+++ src/main/java/org/folio/marccat/dao/GlobalVariableDAO.java
@@ -0,0 +1,38 @@
+package org.folio.marccat.dao;
+
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Session;
+import org.folio.marccat.dao.persistence.S_SYS_GLBL_VRBL;
+import org.folio.marccat.exception.DataAccessException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+
+/**
+ * Provides access to S_SYS_GLBL_VRBL
+ *
+ * @author paulm
+ * @author carment
+ * @since 1.0
+ */
+public class GlobalVariableDAO extends AbstractDAO {
+
+  /**
+   * Return all global variables
+   *
+   * @param session the hibernate session
+   * @return  all global variables.
+   * @throws DataAccessException in case of data access failure
+   */
+  public HashMap<String, String> getAllGlobalVariable(final Session session) throws HibernateException{
+    final HashMap<String, String> hash = new HashMap<>();
+    final List<S_SYS_GLBL_VRBL> listAllKeys = find(session, "from S_SYS_GLBL_VRBL");
+    final Iterator<S_SYS_GLBL_VRBL> iter = listAllKeys.iterator();
+    while (iter.hasNext()) {
+      S_SYS_GLBL_VRBL rawGlobalVar = iter.next();
+      hash.put(rawGlobalVar.getName(), rawGlobalVar.getValue());
+    }
+    return hash;
+  }
+}
