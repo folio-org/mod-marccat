82c36b20c3227e39165aa0a649aebbd8c6ae7338
carmentrazza <carmen.trazza@atcult.it>
2020-10-29 13:28:54 +0100

    Fix duplicate code

--- src/main/java/org/folio/marccat/dao/BibliographicRelationshipDAO.java
+++ src/main/java/org/folio/marccat/dao/BibliographicRelationshipDAO.java
@@ -77,7 +77,7 @@ public class BibliographicRelationshipDAO extends AbstractDAO {
     return (!singleView.isEmpty()) ? (BibliographicRelationship) singleView.get(0) : null;
   }
 
- 
+
   /**
    * Builds the relation string text.
    *
@@ -99,64 +99,19 @@ public class BibliographicRelationshipDAO extends AbstractDAO {
     if (t instanceof NameAccessPoint) {
       stringText.addSubfield(new Subfield("a", t.getStringText().toDisplayString()));
     }
-
-    t = (VariableField) item.findFirstTagByNumber("130");
-
-    if (t != null) {
-      stringText.addSubfield(new Subfield("t", t.getStringText().toDisplayString()));
-    } else {
-      t = (VariableField) item.findFirstTagByNumber("245");
-      if (t != null) {
-        stringText.addSubfield(new Subfield("t", t.getStringText().toDisplayString()));
-      }
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("210");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("p", t.getStringText().toDisplayString()));
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("250");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("b", t.getStringText().toDisplayString()));
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("260");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("d", t.getStringText().toDisplayString()));
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("020");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("z", t.getStringText().toDisplayString()));
-    } else {
-      t = (VariableField) item.findFirstTagByNumber("022");
-      if (t != null) {
-        stringText.addSubfield(new Subfield("x", t.getStringText().toDisplayString()));
-      }
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("088");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("r", t.getStringText().toDisplayString()));
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("027");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("u", t.getStringText().toDisplayString()));
-    }
-
-    t = (VariableField) item.findFirstTagByNumber("030");
-    if (t != null) {
-      stringText.addSubfield(new Subfield("y", t.getStringText().toDisplayString()));
-    }
-
+    getTitle(stringText, item);
     stringText.addSubfield(new Subfield("e", item.getBibItmData().getLanguageCode()));
     stringText.addSubfield(new Subfield("f", item.getBibItmData().getMarcCountryCode()));
     return stringText;
   }
 
-
+  private void getTitle(StringText stringText, BibliographicItem item) {
+    VariableField t;
+    t = (VariableField) item.findFirstTagByNumber("245");
+    if (t != null) {
+      stringText.addSubfield(new Subfield("t", t.getStringText().toDisplayString()));
+    }
+  }
 
 
 }
