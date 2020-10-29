bbc3b66811f7d774c4bb626ea70ac3c541c50744
carmentrazza <carmen.trazza@atcult.it>
2020-10-29 15:00:54 +0100

    Fix other duplicate lines

--- src/main/java/org/folio/marccat/dao/BibliographicRelationshipDAO.java
+++ src/main/java/org/folio/marccat/dao/BibliographicRelationshipDAO.java
@@ -11,7 +11,6 @@ import org.folio.marccat.dao.persistence.BibliographicRelationship;
 import org.folio.marccat.dao.persistence.NameAccessPoint;
 import org.folio.marccat.model.Subfield;
 import org.folio.marccat.util.StringText;
-
 import java.util.List;
 
 /**
@@ -99,18 +98,41 @@ public class BibliographicRelationshipDAO extends AbstractDAO {
     if (t instanceof NameAccessPoint) {
       stringText.addSubfield(new Subfield("a", t.getStringText().toDisplayString()));
     }
-    getTitle(stringText, item);
+    boolean isAddedTitle = appendSubfield(stringText, item, "130", "t");
+    if(!isAddedTitle)
+      appendSubfield(stringText, item, "245", "t");
+    appendSubfield(stringText, item, "210", "p");
+    appendSubfield(stringText, item, "250", "b");
+    appendSubfield(stringText, item, "260", "d");
+    boolean isAddedISBN = appendSubfield(stringText, item, "020", "z");
+    if(!isAddedISBN)
+      appendSubfield(stringText, item, "022", "x");
+    appendSubfield(stringText, item, "088", "r");
+    appendSubfield(stringText, item, "027", "u");
+    appendSubfield(stringText, item, "030", "y");
     stringText.addSubfield(new Subfield("e", item.getBibItmData().getLanguageCode()));
     stringText.addSubfield(new Subfield("f", item.getBibItmData().getMarcCountryCode()));
     return stringText;
   }
 
-  private void getTitle(StringText stringText, BibliographicItem item) {
+
+  /**
+   * Append subfield.
+   *
+   * @param stringText the string text
+   * @param item the item
+   * @param tagNumber the tag number
+   * @param code the code
+   * @return true, if successful
+   */
+  private boolean appendSubfield(final StringText stringText, final BibliographicItem item, final String tagNumber, final String code) {
     VariableField t;
-    t = (VariableField) item.findFirstTagByNumber("245");
+    t = (VariableField) item.findFirstTagByNumber(tagNumber);
     if (t != null) {
-      stringText.addSubfield(new Subfield("t", t.getStringText().toDisplayString()));
+      stringText.addSubfield(new Subfield(code, t.getStringText().toDisplayString()));
+      return true;
     }
+    return false;
   }
 
 
