43331d6f0843ca3630260628620777241644e18f
Elena Turienzo <eturienzo@ibaiscanbit.com>
2020-10-30 13:55:43 +0100

    MODCAT-219: Create an API for the select for authority 008 tag

--- src/test/java/org/folio/marccat/resources/HeaderTypeTest.java
+++ src/test/java/org/folio/marccat/resources/HeaderTypeTest.java
@@ -1,5 +1,8 @@
 package org.folio.marccat.resources;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.Map;
 
 import org.folio.marccat.StorageTestSuite;
 import org.folio.marccat.TestBase;
@@ -8,31 +11,27 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class HeaderTypeTest extends TestBase {
 
- @Test
+  @Test
+
   public void getDocumentCountById() {
 
-    String url = getURI( "/marccat/header-types");
+    String url = getURI("/marccat/header-types");
     Map<String, String> headers = addDefaultHeaders(url, StorageTestSuite.TENANT_ID);
 
-    given()
-      .param("code", "1")
-      .param("lang", "ita")
-      .headers(headers)
-      .when()
-      .get(url)
-      .then()
-      .statusCode(200);
+    given().param("code", "1").param("lang", "ita").headers(headers).when().get(url).then().statusCode(200);
   }
 
+  @Test
+  public void getAuthorityHeadingTypes() {
 
+    String url = getURI("/marccat/auth-header-types");
+    Map<String, String> headers = addDefaultHeaders(url, StorageTestSuite.TENANT_ID);
 
+    given().param("code", "008").param("lang", "eng").headers(headers).when().get(url).then().statusCode(200);
+  }
 
 }
